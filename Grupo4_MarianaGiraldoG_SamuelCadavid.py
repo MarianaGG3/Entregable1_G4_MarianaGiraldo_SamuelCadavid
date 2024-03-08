e65d99eee92efc8078bbe2e9e6fd7b414f3015e4
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-03-07 22:45:13 -0500

    first commit

---
+++ Grupo4_MarianaGiraldoG_SamuelCadavid.py
@@ -0,0 +1,14 @@
+class Protesiscadera:
+    pass
+#prótesis de cadera, marcapasos cardíacos, stents coronarios, implantes dentales y prótesis de rodilla
+
+class Marcapasos:
+    pass
+
+class Stents:
+    pass
+class ImplantesDentales:
+    pass
+
+class ProtesisRodilla:
+    pass
