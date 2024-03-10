b1db6d3467342912b1006ee5b7b3841b9253d935
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-03-10 18:01:26 -0500

    menu vacio-asociacion clase padre

--- Grupo4_MarianaGiraldoG_SamuelCadavid.py
+++ Grupo4_MarianaGiraldoG_SamuelCadavid.py
@@ -1,14 +1,53 @@
-class Protesiscadera:
+class Dispositivos:
+    def __init__(self):
+        self.__nombre=""
+        self.__material=""
+        self.__tamaño=0
+
+
+class Protesiscadera(Dispositivos):
     pass
 
 
-class Marcapasos:
+class Marcapasos(Dispositivos):
     pass
 
-class Stents:
+class Stents (Dispositivos):
     pass
-class ImplantesDentales:
+class ImplantesDentales(Dispositivos):
     pass
 
-class ProtesisRodilla:
+class ProtesisRodilla(Dispositivos):
     pass
+
+# def validar(msj):
+#     while True:
+#         try:
+#             valor = int(input(msj))
+#             break
+#         except ValueError:
+#             print("Ingrese un dato numérico...")
+
+dispos=Dispositivos
+while True:
+    menu=int(input("""ingrese la opcion que dese seleccionar:
+    1. Ingresar un nuevo implante
+    2. Eliminar implante
+    3. Editar información del implante 
+    4. Visualizar el inventario completo
+    5. Salir del sistema
+    """))
+
+    if menu==5:
+        break
+    elif menu==2:
+        pass
+    elif menu==3:
+        pass
+    elif menu==4:
+        pass
+    elif menu==1:
+        pass
+
+    else:
+        print("ingrese una opcion")
