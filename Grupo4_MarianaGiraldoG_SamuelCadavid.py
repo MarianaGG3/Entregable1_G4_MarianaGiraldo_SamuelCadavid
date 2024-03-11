b1058d174f88b8371b9058317737ab9597875d8a
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-03-11 16:25:23 -0500

    asignacion variables a clases

--- Grupo4_MarianaGiraldoG_SamuelCadavid.py
+++ Grupo4_MarianaGiraldoG_SamuelCadavid.py
@@ -1,24 +1,56 @@
-class Dispositivos:
-    def __init__(self):
-        self.__nombre=""
-        self.__material=""
-        self.__tamaño=0
+class Implantes: #Clase padre de Implantes
+    def __init__(self, precio, distribuidor, estado ):
+        
+        self.__precio=precio
+        self.__distribuidor=distribuidor
+        self.__estado=estado
 
 
-class Protesiscadera(Dispositivos):
-    pass
+class Protesiscadera(Implantes):
+   def __init__(self, precio, distribuidor, estado, tipofij, tamano, material):
+        super().__init__(self, precio, distribuidor, estado)
+        self.__tipofij=tipofij
+        self.__tamano=tamano
+        self.__material=material
+        
+
+class Marcapasos(Implantes):
+    def __init__(self, precio, distribuidor, estado, frec, nelectrodos, alambrico):
+        super().__init__(self, precio, distribuidor, estado)
+        self.__distribuidor=distribuidor
+        self.__estado=estado
+        self.__frec=frec
+        self.__nelectrodos=nelectrodos
+        self.__alambrico=alambrico
 
 
-class Marcapasos(Dispositivos):
-    pass
 
-class Stents (Dispositivos):
-    pass
-class ImplantesDentales(Dispositivos):
-    pass
+class Stents (Implantes):
+    def __init__(self, precio, distribuidor, estado, longitud, diametro, material):
+        super().__init__(self, precio, distribuidor, estado)
+        self.__longitud=longitud
+        self.__diametro=diametro
+        self.__material=material
 
-class ProtesisRodilla(Dispositivos):
-    pass
+
+
+
+class ImplantesDentales(Implantes):
+    def __init__(self, precio, distribuidor, estado, forma, sistfij, material):
+        super().__init__(self, precio, distribuidor, estado)
+        self.__forma=forma
+        self.__sistfij=sistfij
+        self.__material= material
+
+
+
+class ProtesisRodilla(Implantes):
+    def __init__(self, precio, distribuidor, estado, tipofij, tamano, material):
+        super().__init__(self, precio, distribuidor, estado)
+        self.__tipofij=tipofij
+        self.__tamano=tamano
+        self.__material=material
+
 
 # def validar(msj):
 #     while True:
@@ -28,7 +60,7 @@ class ProtesisRodilla(Dispositivos):
 #         except ValueError:
 #             print("Ingrese un dato numérico...")
 
-dispos=Dispositivos
+imp=Implantes
 while True:
     menu=int(input("""ingrese la opcion que dese seleccionar:
     1. Ingresar un nuevo implante
@@ -38,16 +70,16 @@ while True:
     5. Salir del sistema
     """))
 
-    if menu==5:
-        break
+    if menu==1:
+        pass
     elif menu==2:
         pass
     elif menu==3:
         pass
     elif menu==4:
         pass
-    elif menu==1:
-        pass
+    elif menu==5:
+        break
 
     else:
         print("ingrese una opcion")
