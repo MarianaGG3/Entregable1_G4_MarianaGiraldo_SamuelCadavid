c8b0040bdfda7b2fc89966715b6352520e5463cc
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-03-11 20:13:33 -0500

    setters y getters de los metodos de las clases

--- Grupo4_MarianaGiraldoG_SamuelCadavid.py
+++ Grupo4_MarianaGiraldoG_SamuelCadavid.py
@@ -5,6 +5,26 @@ class Implantes: #Clase padre de Implantes
         self.__distribuidor=distribuidor
         self.__estado=estado
 
+        #getters: permiten ver los objetos de la clase
+
+        def verPrecio(self):
+            return self.__precio
+
+        def verDistribuidor(self):
+            return self.__distribuidor
+        
+        def verEstado(self):
+            return self.__estado
+        
+        #setter: asignan los objetos
+        def asignarPrecio (self, precio):
+            self.__precio= precio
+
+        def asignarDistribuidor (self, distribuidor):
+            self.__distribuidor=distribuidor
+        
+        def asignarEstado (self, estado):
+            self.__estado=estado
 
 class Protesiscadera(Implantes):
    def __init__(self, precio, distribuidor, estado, tipofij, tamano, material):
@@ -12,18 +32,58 @@ class Protesiscadera(Implantes):
         self.__tipofij=tipofij
         self.__tamano=tamano
         self.__material=material
+
+        #getters de la clase protesis cadera:
+
+        def verTipofij(self):
+            return self.__tipofij
+
+        def verTamano(self):
+            return self.__tamano
+
+        def verMaterial(self):
+            return self.__material
+    
+        #setters: asignacion de los atributos 
+
+        def asignarTipofij (self, tipofij):
+            self.__tipofij=tipofij
+
+        def asignarTamano (self, tamano):
+            self.__tamano=tamano
+
+        def asignarMaterial (self, material):
+            self.__material=material
         
 
 class Marcapasos(Implantes):
     def __init__(self, precio, distribuidor, estado, frec, nelectrodos, alambrico):
         super().__init__(self, precio, distribuidor, estado)
-        self.__distribuidor=distribuidor
-        self.__estado=estado
         self.__frec=frec
         self.__nelectrodos=nelectrodos
         self.__alambrico=alambrico
 
 
+    # getters
+        def verFrec(self):
+            return self.__frec
+
+        def verNElectrodos(self):
+            return self.__nelectrodos
+        def verAlamb(self):
+            return self.__alambrico
+
+    #setters
+
+        def asignarFrec (self, frec):
+            self.__frec=frec
+
+        def asignarNElectrodos (self, nelectrodos):
+            self.__nelectrodos=nelectrodos
+
+        def asignarAlamb (self, alambrico):
+            self.__alambrico=alambrico
+    
 
 class Stents (Implantes):
     def __init__(self, precio, distribuidor, estado, longitud, diametro, material):
@@ -32,8 +92,28 @@ class Stents (Implantes):
         self.__diametro=diametro
         self.__material=material
 
+    #getters
+
+    def verLong(self):
+            return self.__longitud
+     
+    def verDiam(self):
+            return self.__diametro
+
+    def verMaterial(self):
+            return self.__material
+
+    #setters
+
+    def asignarLong (self, longitud):
+            self.__longitud=longitud
+
+    def asignarDiam (self, diametro):
+            self.__diametro=diametro
 
 
+    def asignarMaterial (self, material):
+            self.__material=material
 
 class ImplantesDentales(Implantes):
     def __init__(self, precio, distribuidor, estado, forma, sistfij, material):
@@ -42,7 +122,27 @@ class ImplantesDentales(Implantes):
         self.__sistfij=sistfij
         self.__material= material
 
+        #getters
 
+    def verForma(self):
+            return self.__forma
+    
+    def verSistFij(self):
+            return self.__sistfij
+    
+    def verMaterial(self):
+            return self.__material
+
+        #setters
+
+    def asignarForma (self, forma):
+            self.__forma=forma
+
+    def asignarSistFij (self, sistfij):
+            self.__sistfij=sistfij
+
+    def asignarMaterial (self, material):
+            self.__material=material
 
 class ProtesisRodilla(Implantes):
     def __init__(self, precio, distribuidor, estado, tipofij, tamano, material):
@@ -50,6 +150,29 @@ class ProtesisRodilla(Implantes):
         self.__tipofij=tipofij
         self.__tamano=tamano
         self.__material=material
+
+        #getters
+
+    def verTipofij(self):
+            return self.__tipofij
+
+    def verTamano(self):
+            return self.__tamano
+
+    def verMaterial(self):
+            return self.__material
+
+        #setters
+
+    def asignarTipofij (self, tipofij):
+            self.__tipofij=tipofij
+
+    def asignarTamano (self, tamano):
+            self.__tamano=tamano
+
+    def asignarMaterial (self, material):
+            self.__material=material
+
 
 
 # def validar(msj):
